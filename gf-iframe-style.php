daa9288dc80f7d8d3e2f97a545a9dcd59c2b6448
infomixa <infomixa@gmail.com>
2018-10-09 15:56:03 -0700

    initial

---
+++ gf-iframe-style.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: GF Iframe Style
+*/
+
+function gfIframeStyle($form_id) { ?>
+<!-- this is HTML -->
+<style>
+    input[type="submit"].gform_button {
+    background: #2b5e7c;
+    color: #fff;
+    font-size: 18px !important;
+    height: 60px;
+    width: 100% !important;
+}
+
+.gform_wrapper input[type=text]
+    height: 50px;
+    border: 3px solid #dbdbdb;
+    border-radius: 5px;
+    outline: none; 
+    color: #000;
+
+}
+
+.gform_wrapper textarea {
+    border: 3px solid #dbdbdb;
+    border-radius: 5px;
+    outline: none; 
+    color: #000;
+}
+    
+.gform_wrapper input[type=radio] {
+    border: 3px solid #dbdbdb;
+    border-radius: 5px;
+}
+    
+</style>
+<?php }
+add_action('gfiframe_head', 'gfIframeStyle');
+
+
