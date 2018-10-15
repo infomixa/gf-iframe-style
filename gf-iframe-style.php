97e4d9646112eea29423d5fac9da52fb85fa420c
infomixa <infomixa@gmail.com>
2018-10-15 12:48:52 -0700

    Added Font Awesome CSS Link

--- gf-iframe-style.php
+++ gf-iframe-style.php
@@ -34,5 +34,6 @@ input[type="submit"].gform_button {
 }
     
 </style>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 <?php }
 add_action('gfiframe_head', 'gfIframeStyle');
